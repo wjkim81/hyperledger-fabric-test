bc277ba9b91a1e30e1a742b212310240ed1d0dec
wjkim81 <onejean81@gmail.com>
2017-11-24 13:34:14 +0900

    Modified codes

--- trace-app/start.sh
+++ trace-app/start.sh
@@ -6,20 +6,24 @@
 #
 # Exit on first error
 
-set -e
+set -ev
 
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 
 starttime=$(date +%s)
 
-# launch network; create channel and join peer to channel
+# launch network; 
 cd ../basic-network
-./start.sh
+
+docker-compose -f docker-compose.yml down
+
+docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
+
 
 # Now launch the CLI container in order to install, instantiate chaincode
 # and prime the ledger with our 10 tuna catches
 docker-compose -f ./docker-compose.yml up -d cli
 
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n trace-app -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
-
+#docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n trace-app -c '{"function":"queryCow","Args":["002021864077"]}'
+sleep 10
